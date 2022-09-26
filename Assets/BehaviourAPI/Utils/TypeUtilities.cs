475a516e96955702456b12d13b6e3600c85f5a2f
AlejandroOrejudo2000 <alexgamedev2000@gmail.com>
2022-09-26 13:33:42 +0200

    FEAT - Type utilities

---
+++ Assets/BehaviourAPI/Utils/TypeUtilities.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace BehaviourAPI.Utils
+{
+    public static class TypeUtilities
+    {
+        public static TypeNode GetHierarchyOfType(Type rootType)
+        {
+            return new TypeNode(rootType, GetAllTypesDerivedFrom(rootType));
+        }
+
+        public static Assembly[] GetAllAssemblies() => AppDomain.CurrentDomain.GetAssemblies();
+
+        public static List<Type> GetAllTypesDerivedFrom(Type rootType)
+        {
+            List<Type> allTypes = new List<Type>();
+            GetAllAssemblies().ToList().ForEach(assembly => allTypes.AddRange(assembly.GetTypes()));
+            List<Type> derivedTypes = allTypes.Where((type) => type.IsSubclassOf(rootType)).ToList();
+            return derivedTypes;
+        }
+    }
+
+    public class TypeNode
+    {
+        public Type Type { get; private set; }
+        public List<TypeNode> Childs { get; private set; }
+        public TypeNode(Type rootType, List<Type> derivedTypes)
+        {
+            Type = rootType;
+            Childs = new List<TypeNode>();
+
+
+            foreach (var c in derivedTypes)
+            {
+                var derived = derivedTypes.Where(t => t.IsSubclassOf(rootType));
+                Childs.Add(new TypeNode(c, derived.ToList()));
+            }
+        }
+
+        public override String ToString()
+        {
+            string str = $"{Type.Name} (";
+            Childs.ForEach(t => str += t.ToString());
+            str += ")";
+            return str;
+        }
+    }
+}
